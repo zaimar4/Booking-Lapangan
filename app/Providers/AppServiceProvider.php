b89d63f0770f064e85346cc2d8c57b16d53121a7
Zaim <zaimarif48@gmail.com>
2026-05-16 17:53:33 +0700

    call supabase img

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        
+    if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         // Bagikan data ke semua view secara otomatis
         View::composer('*', function ($view) {
             try {
