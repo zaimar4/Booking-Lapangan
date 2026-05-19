5f437fea708c617bab0df47b895058fa9efb1b19
Zaim <zaimarif48@gmail.com>
2026-05-19 19:30:51 +0700

    update admin to Admin

--- resources/views/admin/detaillapangan.blade.php
+++ resources/views/admin/detaillapangan.blade.php
@@ -149,7 +149,7 @@
                     <div class="px-6 py-5 border-b border-green-50">
                         <div class="text-xs font-medium text-green-400 uppercase tracking-wider mb-1.5">Jenis Lapangan</div>
                         <span class="inline-block text-xs font-semibold text-green-700 bg-green-50 border border-green-200 rounded-md px-3 py-1">
-                            {{ $lapangan->jenis_lapangan }}
+                            {{ $lapangan->jenis_lapangan->nama_jenis }}
                         </span>
                     </div>
 
